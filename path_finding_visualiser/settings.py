afe0f45ec82dc42dd40ccabbeb88a696ff655751
Bora Buyukoz <bbprogit@gmail.com>
2020-08-14 12:55:04 +0300

    Updated static root

--- path_finding_visualiser/settings.py
+++ path_finding_visualiser/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(dir_path, 'pfv/static')
 
 STATIC_URL = '/static/'
