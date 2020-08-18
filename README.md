5ba741fb2be799f0223cde134ee252c9e286f622
Bora Buyukoz <bbprogit@gmail.com>
2020-08-18 23:39:57 +0300

    Updated Readme, Added AWS configurations

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Famous path finding and maze generation algorithms are visualized via a Python-Django app
 
 ## Heroku-App URL: 
+[Click for heroku app!](https://path-maze-visualization.herokuapp.com]) 
+*Currently the heroku app is not working due to the AWS static file configurations.*
--- path_finding_visualiser/settings.py
+++ path_finding_visualiser/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,3 +130,11 @@ USE_TZ = True
 # print("STATIC_ROOT: ", STATIC_ROOT)
 
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
+
+# STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+
+# STATICFILES_STORAGE = 
