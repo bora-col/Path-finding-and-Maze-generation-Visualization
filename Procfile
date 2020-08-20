00a24099272a8b347ee2001feb3ac61c920b70ce
Bora Buyukoz <bbprogit@gmail.com>
2020-08-21 01:06:38 +0300

    Added gunicorn to Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn path_finding_visualiser.wsgi
--- path_finding_visualiser/settings.py
+++ path_finding_visualiser/settings.py
@@ -32,8 +32,6 @@ DEBUG = (os.environ.get('DEBUG_VALUE')=='True')
 
 ALLOWED_HOSTS = ['path-maze-visualization.herokuapp.com', 'localhost', '127.0.0.1']
 
-DISABLE_COLLECTSTATIC=1
-
 
 # Application definition
 
